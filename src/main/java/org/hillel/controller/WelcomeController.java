8ce04d9382e3fd9a3271c08bc108fa0fa458b879
alezalez8 <5446943@ukr.net>
2021-05-18 19:37:57 +0300

    lesson 11 configuration pom.xml

--- src/main/java/org/hillel/controller/WelcomeController.java
+++ src/main/java/org/hillel/controller/WelcomeController.java
@@ -1,7 +1,9 @@
 package org.hillel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WelcomeController {
@@ -11,7 +13,8 @@ public class WelcomeController {
         return "welcome";  // map to welcome.jsp
     }
 
-    @RequestMapping("/auth")
+
+    @RequestMapping(path = "/auth", method = RequestMethod.POST) // for example
     public String authPage() {
         return "login";
     }
