0968860f3fa26dae6f9ce37b6081858b53260d62
alezalez8 <5446943@ukr.net>
2021-06-24 17:54:37 +0300

    lesson 12 add sql_user

--- src/main/java/org/hillel/config/SecurityConfig.java
+++ src/main/java/org/hillel/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package org.hillel.config;
 
+import net.bytebuddy.asm.Advice;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
@@ -17,4 +24,17 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated().and().formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());  // it means we can invoke others methods from another appl.
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // use only for learning and testing
+    }
+
+   /* @Bean
+    public UserDetailsService userDetailsServiceInMemory() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin").password("123").roles("ADMIN", "TICKET").build(),
+                User.withUsername("test").password("test").authorities("ROLE_TICKET").build()); // the same as roles("TICKET")
+    }*/
+
 }
