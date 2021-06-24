1c4af8e94d7ec3d1c87e698a2a1275431b29477d
alezalez8 <5446943@ukr.net>
2021-06-24 21:00:09 +0300

    lesson 12 2.55.28

--- src/main/java/org/hillel/service/security/CustomUserDetailService.java
+++ src/main/java/org/hillel/service/security/CustomUserDetailService.java
@@ -1,12 +1,18 @@
 package org.hillel.service.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.util.Pair;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
     // можно пойти в БД, вначале получаю имя, затем гранты
@@ -20,10 +26,15 @@ public class CustomUserDetailService implements UserDetailsService {
 
         Pair<String, Boolean> paswordAndStatus =
                 jdbcTemplate.queryForObject("select pwd, active from client where email = ?",
-                        (row, iteration) -> Pair.of(row.getString(1), row.getBoolean(2)),
-                        email);
+                        (row, iteration) -> Pair.of(row.getString(1), row.getBoolean(2)), email);
 
+        List<String> authorities = jdbcTemplate.queryForList("select authority from authorities where username = ?",
+                String.class, email);
 
-        return null;
+        User.UserBuilder builder = User.builder();
+        return builder.password(paswordAndStatus.getFirst()).username(email)
+                .disabled((!paswordAndStatus.getSecond()))
+                .authorities(authorities.toArray(String[]::new)).build();
+
     }
 }
